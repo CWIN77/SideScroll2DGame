264254a59f8d729c60afead996d77747c6d8807b
CWIN77 <cwstbp0426@gmail.com>
2022-08-14 14:54:43 +0900

    add orange item

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,12 +5,14 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
   private Rigidbody2D rb;
+  private BoxCollider2D coll;
   private Animator anim;
   private SpriteRenderer spriteRenderer;
   private float dirX;
   private MovementState state;
   [SerializeField] private float moveSpeed = 7f;
   [SerializeField] private float jumpForce = 14f;
+  [SerializeField] private LayerMask jumpableGround;
 
   private enum MovementState { idle, run, jump, fall }
 
@@ -18,6 +20,7 @@ public class PlayerMovement : MonoBehaviour
   private void Start()
   {
     rb = GetComponent<Rigidbody2D>();
+    coll = GetComponent<BoxCollider2D>();
     anim = GetComponent<Animator>();
     spriteRenderer = GetComponent<SpriteRenderer>();
   }
@@ -29,7 +32,7 @@ public class PlayerMovement : MonoBehaviour
 
     rb.velocity = new Vector2(dirX * moveSpeed, rb.velocity.y);
 
-    if (Input.GetButtonDown("Jump") && (int)state != 2 && (int)state != 3)
+    if (Input.GetButtonDown("Jump") && IsGrounded())
     {
       rb.velocity = new Vector2(rb.velocity.x, jumpForce);
     }
@@ -63,6 +66,6 @@ public class PlayerMovement : MonoBehaviour
 
   private bool IsGrounded()
   {
-    
+    return Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.down, .1f, jumpableGround);
   }
 }
